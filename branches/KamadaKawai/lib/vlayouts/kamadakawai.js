e05605e5a51ee9a72b29289a38303f8b2982a922
anze.staric <anze.staric@ef430980-05c2-11de-85a1-fd18384f7567>
2009-03-12 20:27:53 +0000

    distance matrix for positive-weight graphs

--- branches/KamadaKawai/lib/vlayouts/kamadakawai.js
+++ branches/KamadaKawai/lib/vlayouts/kamadakawai.js
@@ -30,25 +30,21 @@ function sortVertex(v1, v2)
  */
 KamadaKawaiVertexLayout.prototype.__djikstraFindShortestPaths = function(graph, vertex)
 {
-  var v = graph.firstVertex();
-  
   //reset all node weights to -1
-  while (v != null) {
-    v.weight = -1;
-    v = graph.nextVertex(v);
+  for (var i1 in graph.vertices) {
+    graph.vertices[i1].weight = -1;
   }
   
   vertex.weight = 0;
   
   var queue = new Array();
   queue.push(vertex);
-  alert(queue);  
   while (queue.length > 0) {
     var a = queue.pop();
     
-    alert(a + " [" + queue + "]");    
-    var e = graph.firstEdge(a);
-    while (e != null) {
+    for (var i2 in a.edges) {
+      e = a.edges[i2];
+      
       var b = e.endVertex;
       var w = a.weight + e.weight;
       if (b.weight == -1) {        
@@ -57,22 +53,53 @@ KamadaKawaiVertexLayout.prototype.__djikstraFindShortestPaths = function(graph, 
       } else if (b.weight > w) {
         b.weight = w;
       }
-      e = graph.nextEdge(e);
     }
-    queue.sort(sortVertex);
-    alert(" [" + queue + "] (" + queue.length + ")");  
+    
+    for (var i5 in a.reverseEdges) {
+      e = a.reverseEdges[i5];
+      
+      var b = e.endVertex;
+      var w = a.weight + e.weight;
+      if (b.weight == -1) {        
+        b.weight = w;
+        queue.push(b);
+      } else if (b.weight > w) {
+        b.weight = w;
+      }
+    }
+    
+    queue.sort(sortVertex); 
   }
   
   result = new Array();
-  v = graph.firstVertex();
-  i = 0;  
-  while (v != null) {
-    result[i] = v.weight;
-    i++;
-    v = graph.nextVertex(v);
+  for (i3 in graph.vertices) {
+    var v = graph.vertices[i3]
+    result[i3] = v.weight;
   }
   return result;
 }
+
+/**
+ * Calculates the shortest paths from vertex to any other
+ * connected vertex
+ *  
+ * @param graph A valid graph instance
+ * @param vertex A valid vertex instance
+ */
+KamadaKawaiVertexLayout.prototype.__computeEdgeLength = function(graph, distanceMatrix, side)
+{
+  //edge length is side length divided by largest distance in graph
+  var result = 0;
+  for (var i1 in distanceMatrix)
+    for (var i2 in distanceMatrix[i1])
+      if (distanceMatrix[i1][i2] > result)
+        result = distanceMatrix[i1][i2];
+  
+  return side / result;
+
+}
+
+this.spring_constant = 5;
 
 /**
  * Calculates the coordinates based on Kamada-Kawai spring
@@ -86,14 +113,28 @@ KamadaKawaiVertexLayout.prototype.layout = function(graph)
   //Create distance matrix
     //Johnson's algorithm to find shortest paths between all vertices should be implemented
     //http://en.wikipedia.org/wiki/Johnson's_algorithm
-  var v = graph.firstVertex();
-  var minDistanceMatrix = new Array();
-  i = 0;
-  while (v != null) {
-    minDistanceMatrix[i] = this.__djikstraFindShortestPaths(graph, v);
-    i++;
-    v = graph.nextVertex(v);
-  }  
+  var distance = new Array();
+  var txt = "";
+  for (var i4 in graph.vertices) {
+    var v = graph.vertices[i4];
+    distance[i4] = this.__djikstraFindShortestPaths(graph, v);
+  }
+  
+  var edge_length = __computeEdgeLength(graph, distance, width);
+  
+  var spring_strength = new Array();
+  
+  var K = this.spring_constant;
+  
+  for (var i1 in graph.vertices) {
+    spring_strength[i1] = new Array();
+    for (var i2 in graph.vertices) {
+      
+    }
+  } 
+  
+  
+  
   
   //Do some voodoo magic with derivates
 }
